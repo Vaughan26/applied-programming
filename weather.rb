6e715421795e668123dabe00ed31b3d5b52d63f8
Vaughan26 <Mfvaughan26@gmail.com>
2020-02-18 01:46:18 +0000

    week 7

--- weather.rb
+++ weather.rb
@@ -1,7 +1,7 @@
 require "forecast_io"
 
 # configure the Dark Sky API with your API key
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "ca4d3ae98d02f415fdf7793794f0f224"
 
 # do the heavy lifting, use Global Hub lat/long
 forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
@@ -9,6 +9,16 @@ forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
 # pp = pretty print
 # use instead of `puts` to make reading a hash a lot easier
 # e.g. `pp forecast`
+current_temperature = forecast ["currently"]["temperature"]
+
+conditions = forecast["currently"]["summary"]
+puts "In Evanston, it is currently #{current_temperature} and #{conditions}"
+High_temperature = forecast["daily"]["data"][0]["temperatureHigh"]
+puts High_temperature
+
+for day in forecast ["daily"]["data"]
+   puts "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}."
+end
 
 # Example output:
 #
@@ -22,3 +32,5 @@ forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
 # A high temperature of 40.42 and Clear throughout the day.
 # A high temperature of 23.21 and Overcast throughout the day.
 # A high temperature of 29.12 and Clear throughout the day.
+
+
